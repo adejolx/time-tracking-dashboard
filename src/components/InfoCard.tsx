e198e532748d10bd81149680a4f06858ad5e0332
adejolx <recursr.dev@gmail.com>
2023-05-06 11:58:30 +0100

    fix: properly name attribute to camelCase

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -42,7 +42,7 @@ const InfoCard = ({ state, datum, bgStyles }: Props<TrackerData>) => {
               <path
                 d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z"
                 fill="#BBC0FF"
-                fill-rule="evenodd"
+                fillRule="evenodd"
               />
             </svg>
           </button>
